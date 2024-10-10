f2c0998531c2a049509305cc85f111e1cf19401d
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-10 17:16:23 +0200

    added fields and construct on class Category

--- classes/Category.php
+++ classes/Category.php
@@ -1,2 +1,16 @@
 <?php
-class Category extends Product {}
+class Category extends Product
+{
+    public string $nameCategory;
+
+    public function __construct(
+        string $nameProduct,
+        float $price,
+        string $image,
+        string $nameCategory
+    ) {
+        parent::__construct($nameProduct, $price, $image);
+
+        $this->nameCategory = $nameCategory;
+    }
+}
