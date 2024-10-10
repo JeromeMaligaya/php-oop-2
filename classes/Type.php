3a0eca18f2e9d5a7c801982f156f107615f20d4d
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-10 17:17:48 +0200

    added fields and construct on class Type

--- classes/Type.php
+++ classes/Type.php
@@ -1,2 +1,17 @@
 <?php
-class Type extends Category {}
+class Type extends Category
+{
+    public string $nameType;
+
+    public function __construct(
+        string $nameProduct,
+        float $price,
+        string $image,
+        string $nameCategory,
+        string $nameType,
+    ) {
+        parent::__construct($nameProduct, $price, $image, $nameCategory);
+
+        $this->nameType = $nameType;
+    }
+}
