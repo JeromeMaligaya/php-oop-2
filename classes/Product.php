28ea069f7793fe156d5ba8a4eea57f44e24029a8
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-10 17:14:46 +0200

    added fields and construct on class Product

--- classes/Product.php
+++ classes/Product.php
@@ -1,2 +1,17 @@
 <?php
-class Product {}
+class Product
+{
+    public string $nameProduct;
+    public float $price;
+    public string $image;
+
+    public function __construct(
+        string $nameProduct,
+        float $price,
+        string $image
+    ) {
+        $this->nameProduct = $nameProduct;
+        $this->price = $price;
+        $this->image = $image;
+    }
+}
