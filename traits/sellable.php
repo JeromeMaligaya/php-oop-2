1f38545d993f69966e242ea56bc9113eec8cc34a
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-10-15 22:54:41 +0200

    added on Sellable.php getDiscount() with exception

--- traits/sellable.php
+++ traits/sellable.php
@@ -7,4 +7,14 @@ trait Sellable
     {
         return $this->price;
     }
+
+    public function getDiscount(float $discount_percent): void
+    {
+        if ($discount_percent > 0 && $discount_percent < 100) {
+            $discount = ($this->price * ($discount_percent / 100));
+            $this->price -= $discount;
+        } else {
+            throw new Exception("Insert a correct argument (value range: 1 to 99) on getDiscount()");
+        }
+    }
 }
